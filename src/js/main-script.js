4dfb14dc9695abf8c89e5da257b2df9dbc29938b
Davi Camarinha <davi.camarinha@agenciaecan.com.br>
2018-01-12 14:57:15 -0200

    Zoom

--- src/js/main-script.js
+++ src/js/main-script.js
@@ -1,3 +1,35 @@
 $(document).ready(function(){
-    console.log('teste 123');
+    // zoom
+    var currentMousePos = { x: -1, y: -1 };
+
+$('.main-img')
+    .mouseover(function(){
+        if($(this).find('.zoom').length == 0){
+            $(this).append('<div class="zoom">'+ $(this).html() +'</div>');
+        }
+
+        $('.zoom').show();
+
+        var resize = 150;
+        var origH  = 640;
+        var origW  = 937;
+        var mouseX = event.x;
+        var mouseY = event.y;
+        var newH   = origH * (resize / 100);
+        var newW   = origW * (resize / 100);
+
+        $('.zoom img').width(newH + 'px');
+        $('.zoom img').height(newW + 'px');
+    })
+    .mouseout(function(){
+        $('.zoom').hide();
+    })
+    .mousemove(function(event) {
+        var Xpos = event.offsetX;
+        var Ypos = event.offsetY;
+    
+        $('.zoom img').css('top', '-' +Ypos+ 'px');
+        $('.zoom img').css('left', '-' +Xpos+ 'px');
+    });
+    // 
 });
